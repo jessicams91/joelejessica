6ebbd134bbf5b98ebf967f3169a293bcb9a17b15
jessicams91 <jessicams91@hotmail.com>
2021-02-04 17:24:11 -0300

    change links

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'menu' => 'menu#index'
-  get 'products' => 'products#index'
+  get 'cardapio' => 'menu#index'
+  get 'presentes' => 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
